766d2ff482e16af63b41870832712fc8e839e643
Hung Nguyen <ndhung29301@gmail.com>
2022-04-14 22:30:06 +0700

    change pakage

--- Ecommerce/todo-Ecommerce/src/App.tsx
+++ Ecommerce/todo-Ecommerce/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import styled from "styled-components";
+const Box = styled.div`
+ background-color: #61dafb;
+  height: 400px;
+`;
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+<Box/>
+
+
+
   );
 }
 
