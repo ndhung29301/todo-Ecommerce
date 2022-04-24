89b424ad3cd27409bb241bb0307f6572fdfb861f
Hung Nguyen <ndhung29301@gmail.com>
2022-04-24 23:41:48 +0700

    header

---
+++ Ecommerce/todo-Ecommerce/src/components/organisms/header/header.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import {Box, Button, Flex, Link, Text} from "../../atoms/custom_basic";
+import { MdSearch } from "react-icons/md";
+import styled from "styled-components";
+
+
+
+
+const Categories=styled(Link)`
+  &:hover {
+    text-decoration-line: underline;
+  }
+`
+const Container=styled(Box)`
+  &:hover {
+    background-color: transparent;
+  }
+`
+
+export  const  Header :React.VFC=():JSX.Element =>{
+    return (
+    <Container width={"100%"} backgroundColor={"pink"} paddingBottom={'20px'}>
+    <Flex justifyContent={"space-between"} padding={"30px 50px 20px 50px"} alignItems={"center"}>
+           <Button flexDirection={"row"} border={"none"} color={"none"} backgroundColor={"transparent"} alignItems={"center"} >
+               <MdSearch fontSize={"24px"}/>
+               <Text marginLeft={"7px"} fontSize={"14px"} lineHeight={"20px"} color={"#000000"} >Search</Text>
+           </Button>
+        <Link href={"/home"} textDecorationLine={"none"} fontSize={"28px"} lineHeight={"32px"} fontFamily={"Roboto"} fontWeight={"bold"} color={"#000000"} >
+            LOUIS VUITTON
+        </Link>
+        <Flex alignItems={"center"}>
+            <Link href={"#"} marginRight={"10px"} color={"#000000"} fontSize={"14px"} lineHeight={"24px"} fontFamily={"Roboto"} textDecorationLine={"none"}>
+                Wishlist
+            </Link>
+            <Link textDecorationLine={"none"} color={"#000000"} href={"#"} fontSize={"14px"} lineHeight={"24px"} fontFamily={"Roboto"}>
+                My LV
+            </Link>
+        </Flex>
+    </Flex>
+        <Flex alignItems={"center"} justifyContent={"center"} >
+            <Flex>
+                <Categories textDecorationLine={"initial"} padding={"0 10px 0 10px"} fontWeight={"bold"} color={"#000000"} href={"#"} fontSize={"14px"} lineHeight={"24px"} fontFamily={"Roboto"}>
+                    NEW
+                </Categories>
+                <Categories textDecorationLine={"none"} padding={"0 10px 0 10px"} fontWeight={"bold"} color={"#000000"} href={"#"} fontSize={"14px"} lineHeight={"24px"} fontFamily={"Roboto"}>
+                    WOMEN
+                </Categories>
+                <Categories textDecorationLine={"none"} padding={"0 10px 0 10px"} fontWeight={"bold"} color={"#000000"} href={"#"} fontSize={"14px"} lineHeight={"24px"} fontFamily={"Roboto"}>
+                   MEN
+                </Categories>
+                <Categories textDecorationLine={"none"} padding={"0 10px 0 10px"} fontWeight={"bold"} color={"#000000"} href={"#"} fontSize={"14px"} lineHeight={"24px"} fontFamily={"Roboto"}>
+                    ART OF LIVING
+                </Categories>
+                <Categories textDecorationLine={"none"} padding={"0 10px 0 10px"} fontWeight={"bold"} color={"#000000"} href={"#"} fontSize={"14px"} lineHeight={"24px"} fontFamily={"Roboto"}>
+                    WORLD OF LOUIS VUITTON
+                </Categories>
+            </Flex>
+        </Flex>
+        {/*<Box  padding={"0 20px 0 20px"}>*/}
+        {/*    <Flex border={'1px solid #307df1'}/>*/}
+        {/*</Box>*/}
+
+    </Container>
+
+
+    )
+}
---
+++ Ecommerce/todo-Ecommerce/src/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Header} from "../../components/organisms/header";
+
+export const Home : React.VFC =():JSX.Element => {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
